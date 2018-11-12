f5f798ed86ec07577bfaf64b9e944dcad26e9046
Tim Peoples <tim@toolman.org>
2018-11-12 10:41:14 -0800

    Tests updated for new regex logic

--- scanner_test.go
+++ scanner_test.go
@@ -1,10 +1,18 @@
 package scanner
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
+
+// If emitJS is true, we'll emit JSON for each encountered token instead of
+// actually testing things. Use this to regenerate 'testdata/expected.json"
+// when/if the test input changes significantly.
+const emitJS = false
 
 const (
 	DOIT = iota + 2345
@@ -12,9 +20,17 @@ const (
 	ASSIGN
 	LDANGLE
 	RDANGLE
+	L2BRACE
+	R2BRACE
 )
 
-func TestFoo(t *testing.T) {
+func TestScanner(t *testing.T) {
+	DebugRE = true
+	wantList, err := loadExpected("testdata/expected.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	f, err := os.Open("testdata/input")
 	if err != nil {
 		t.Fatal(err)
@@ -30,39 +46,118 @@ func TestFoo(t *testing.T) {
 	dubls := Doubles{
 		{'<', LDANGLE},
 		{'>', RDANGLE},
+		{'{', L2BRACE},
+		{'}', R2BRACE},
 	}
 
-	var s *Scanner
+	var (
+		tok rune
+		i   int
+		s   *Scanner
+	)
+
 	ef := ErrFunc(func(msg string) { err = fmt.Errorf("%s: %v", msg, s.Position()) })
 	s = New(f, ef, kwrds, pairs, dubls, +ScanHashComments, +ScanTimespans, +ScanStdSizes, -SkipComments, +ScanRegexen)
 
-	var tok rune
-
 	for tok != EOF {
+		err = nil
 		tok = s.Scan()
+		want := wantList[i]
 
-		if err != nil {
-			t.Logf("ERROR: %v\n", err)
-			err = nil
+		got := expTok(s, tok, err)
+
+		if emitJS {
+			js, err := json.Marshal(got)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Println(string(js))
+			// t.Logf("JSON: %s", js)
 			continue
 		}
 
-		var ts string
-		switch tok {
-		case KeyWord:
-			ts = "Keyword"
-		case BRACES:
-			ts = "BRACES"
-		case ASSIGN:
-			ts = "ASSIGN"
-		case LDANGLE:
-			ts = "LDANGLE"
-		case RDANGLE:
-			ts = "RDANGLE"
-		default:
-			ts = s.TokenString(tok)
+		t.Logf("%d: %+v", i, got)
+		if got.TokenString == "Regex" {
+			t.Logf("    %s", showme(got.Text))
 		}
 
-		t.Logf("%s: %q\n", ts, s.Text())
+		i++
+
+		if !got.equal(want) {
+			t.Errorf("token #%d=%d [pos=%v]: got %+v; wanted %+v", i, tok, s.Position(), got, want)
+		}
 	}
+}
+
+func expTok(s *Scanner, tok rune, err error) *expected {
+	if err != nil {
+		return &expected{Err: err.Error()}
+	}
+
+	var ts string
+	switch tok {
+	case KeyWord:
+		ts = "Keyword"
+	case BRACES:
+		ts = "BRACES"
+	case ASSIGN:
+		ts = "ASSIGN"
+	case LDANGLE:
+		ts = "LDANGLE"
+	case RDANGLE:
+		ts = "RDANGLE"
+	default:
+		ts = s.TokenString(tok)
+	}
+
+	return &expected{TokenString: ts, Text: s.Text()}
+}
+
+//--------------------------------------------------------------
+
+type expected struct {
+	TokenString string `json:"token_string,omitempty"`
+	Text        string `json:"text,omitempty"`
+	Err         string `json:"error,omitempty"`
+}
+
+func loadExpected(filename string) ([]*expected, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var exp []*expected
+
+	if err := json.Unmarshal(data, &exp); err != nil {
+		return nil, err
+	}
+
+	return exp, nil
+}
+
+func (x *expected) equal(o *expected) bool {
+	switch {
+	case x == nil && o == nil:
+		return true
+	case x == nil || o == nil:
+		return false
+	case x.Err+o.Err != "" && strings.HasPrefix(x.Err, o.Err):
+		return true
+	case x.Err+o.Err != "" && strings.HasPrefix(o.Err, x.Err):
+		return true
+	case x.TokenString == o.TokenString && x.Text == o.Text:
+		return true
+	default:
+		return false
+	}
+}
+
+func showme(s string) string {
+	var ss []string
+	for _, r := range []rune(s) {
+		ss = append(ss, fmt.Sprintf("'%c'", r))
+	}
+
+	return strings.Join(ss, ", ")
 }
