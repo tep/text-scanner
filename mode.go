6f32d21b1ec1c146bc089a9dea322ea8b609a23f
Tim Peoples <tim@toolman.org>
2018-12-06 08:35:02 -0800

    Add public Eanble and Disable methods

--- mode.go
+++ mode.go
@@ -9,6 +9,20 @@ func (o ScanMode) setOpt(s *Scanner) {
 		s.mode |= uint(o)
 	}
 }
+
+func (s *Scanner) Enable(mode ...ScanMode) {
+	for _, m := range mode {
+		m.setOpt(s)
+	}
+}
+
+func (s *Scanner) Disable(mode ...ScanMode) {
+	for _, m := range mode {
+		m *= -1
+		m.setOpt(s)
+	}
+}
+
 func (s *Scanner) can(m ScanMode) bool {
 	return s.mode&uint(m) != 0
 }
